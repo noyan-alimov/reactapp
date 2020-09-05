822103e651ec0c5cbec128d66ca8c01ea39fdfe5
Noyan Alimov <noyan.alimov@gmail.com>
2020-09-05 13:02:38 +0100

    replaced local state to redux in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,29 @@
 import React, { Component } from 'react';
-import { Product, Order } from "./data/entities";
-import { ProductList } from "./productList";
-
-let testData: Product[] = [1, 2, 3, 4, 5].map(num => 
-  ({ id: num, name: `Prod${num}`, category: `Cat${num % 2}`,
-      description: `Product ${num}`, price: 100 }))
+// import { Product, Order } from "./data/entities";
+// import { ProductList } from "./productList";
+import { dataStore } from "./data/dataStore";
+import { Provider } from "react-redux";
+import { HttpHandler } from "./data/httpHandler";
+import { addProduct } from "./data/actionCreaters";
+import { ConnectedProductList } from "./data/productListConnector";
 
 interface Props {
 
 }
 
-interface State {
-  order: Order
-}
-
-export default class App extends Component<Props, State> {
+export default class App extends Component<Props> {
+  private httpHandler = new HttpHandler();
 
   constructor(props: Props) {
     super(props);
-    this.state = {
-      order: new Order()
-    }
+    this.httpHandler.loadProducts(data => 
+      dataStore.dispatch(addProduct(...data)));
   }
 
   render = () => 
     <div className='App'>
-      <ProductList products={testData} categories={this.categories}
-        order={this.state.order} addToOrder={this.addToOrder} />
+      <Provider store={dataStore}>
+        <ConnectedProductList />
+      </Provider>
     </div>
-
-  get categories(): string[] {
-    return [...new Set(testData.map(p => p.category))]
-  }
-
-  addToOrder = (product: Product, quantity: number) => {
-    this.setState(state => {
-      state.order.addProduct(product, quantity);
-      return state;
-    })
-  }
 }
